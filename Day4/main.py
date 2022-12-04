9cc3952eb679f17077ef6b11f5a471aeb0295d2f
Andrew Stoerkel <astoer1@students.towson.edu>
2022-12-04 01:16:38 -0500

    refactored

--- Day4/main.py
+++ Day4/main.py
@@ -1,16 +1,16 @@
 
 def fully_contains(l1, l2):
-    return (int(l1[1]) in range(int(l2[0]), int(l2[1])+1) and int(l1[0]) in range(int(l2[0]), int(l2[1])+1)) or (int(l2[1]) in range(int(l1[0]), int(l1[1])+1) and int(l2[0]) in range(int(l1[0]), int(l1[1])+1))
+    return (l1[1]) in range(l2[0], l2[1]+1) and l1[0] in range(l2[0], l2[1]+1) or (l2[1] in range(l1[0], l1[1]+1) and l2[0] in range(l1[0], l1[1]+1))
 
 def range_overlaps(l1, l2):
-    return (int(l1[1]) in range(int(l2[0]), int(l2[1])+1) or int(l1[0]) in range(int(l2[0]), int(l2[1])+1)) or (int(l2[1]) in range(int(l1[0]), int(l1[1])+1) or int(l2[0]) in range(int(l1[0]), int(l1[1])+1))
+    return (l1[1] in range(l2[0], l2[1]+1) or l1[0] in range(l2[0], l2[1]+1)) or (l2[1] in range(l1[0], l1[1]+1) or l2[0] in range(l1[0], l1[1]+1))
 
 def main() -> None:
     sum = 0
     with open("input.txt", "r") as f:
         for l in f:
             l = [val.split('-') for val in l.strip().split(',')]
-            if(range_overlaps(l[0], l[1])):
+            if(range_overlaps([int(val) for val in l[0]], [int(val) for val in l[1]])):
                 sum+=1
                 # print(l)
                 # print(fully_contains(l[0], l[1]))
