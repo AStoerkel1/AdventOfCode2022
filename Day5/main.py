d7d2516418694f96a32c729315eb3724020870c9
Andy Stoerkel <astoer1@students.towson.edu>
2022-12-05 09:14:12 -0500

    stacks

---
+++ Day5/main.py
@@ -0,0 +1,57 @@
+
+'''
+[V]     [B]                     [F]
+[N] [Q] [W]                 [R] [B]
+[F] [D] [S]     [B]         [L] [P]
+[S] [J] [C]     [F] [C]     [D] [G]
+[M] [M] [H] [L] [P] [N]     [P] [V]
+[P] [L] [D] [C] [T] [Q] [R] [S] [J]
+[H] [R] [Q] [S] [V] [R] [V] [Z] [S]
+[J] [S] [N] [R] [M] [T] [G] [C] [D]
+ 1   2   3   4   5   6   7   8   9 
+'''
+
+'''
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+'''
+
+dic = {
+    1: ['J','H', 'P', 'M', 'S', 'F', 'N', 'V'],
+    2: ['S', 'R', 'L', 'M', 'J', 'D', 'Q'],
+    3: ['N','Q','D', 'H', 'C', 'S', 'W', 'B'],
+    4: ['R', 'S', 'C', 'L'],
+    5: ['M', 'V', 'T', 'P', 'F', 'B'],
+    6: ['T', 'R', 'Q', 'N', 'C'],
+    7: ['G', 'V', 'R'],
+    8: ['C','Z','S','P','D','L','R'],
+    9: ['D', 'S', 'J','V','G','P','B','F'],
+    'switch': []
+}
+
+def main() -> None:
+    with open("input.txt", "r") as f:
+        for l in f:
+            l = l.strip().split(' ')
+            number = int(l[1])
+            fromStack = int(l[3])
+            toStack = int(l[5])
+            for i in range(number):
+                b = dic[fromStack].pop()
+                dic['switch'].append(b)
+            for i in range(number):
+                b = dic['switch'].pop()
+                dic[toStack].append(b)
+        for i, k in enumerate(dic):
+            if k == 'switch':
+                pass
+            else: 
+                print(dic[k][-1])
+
+
+
+
+if __name__ == "__main__":
+    main()
